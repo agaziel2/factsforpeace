b73ac034e1a8900acf2efa10edc3d762b6a4aa91
agaziel2 <ag@risklambda.com>
2023-11-11 22:13:58 +0100

    fff 2

---
+++ src/DataUploadForm.js
@@ -0,0 +1,34 @@
+// src/components/DataUploadForm.js
+
+import React, { useState } from 'react';
+import Papa from 'papaparse';
+
+function DataUploadForm({ onFileLoaded }) {
+  const [file, setFile] = useState(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (file) {
+      Papa.parse(file, {
+        header: true,
+        complete: (results) => {
+          onFileLoaded(results.data);
+        },
+        skipEmptyLines: true,
+      });
+    } else {
+      alert('Please select a CSV file.');
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <input type="file" onChange={e => setFile(e.target.files[0])} accept=".csv" />
+      </div>
+      <button type="submit">Upload Data</button>
+    </form>
+  );
+}
+
+export default DataUploadForm;
